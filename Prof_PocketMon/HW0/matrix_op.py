4ad80296e1311091a2300aa240006b74ef2632af
MannyHuang <416403086@qq.com>
2020-11-16 17:14:57 +0800

    take ml course of prof.Lee

---
+++ Prof_PocketMon/HW0/matrix_op.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+fileA = open('./Q1/matrixA.txt', 'r')
+fileB = open('./Q1/matrixB.txt', 'r')
+
+strA = fileA.read()
+list_strA = strA.split(',')
+list_intA = list(map(int, list_strA))
+matrixA = np.mat(list_intA)
+
+strB = fileB.read()
+list_strB = strB.split()
+list_intB = [list(map(int, i.split(','))) for i in list_strB]
+matrixB = np.mat(list_intB)
+
+mul = np.matmul(matrixA, matrixB).tolist()
+result = []
+for i in mul:
+    for j in i:
+        result.append(j)
+result = sorted(result, reverse=True)
+output = np.savetxt('answer1.txt',result, fmt='%d')
+
