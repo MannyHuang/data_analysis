fa9d2b09f3008dead06b9813bd1f0f2f40ee627c
MannyHuang <416403086@qq.com>
2020-11-16 19:07:04 +0800

    complete hw0

--- Prof_PocketMon/HW0/Q2/pic_diff.py
+++ Prof_PocketMon/HW0/Q2/pic_diff.py
@@ -1,4 +1,14 @@
 from PIL import Image 
 
 pic1 = Image.open('lena_modified.png')
-print(pic1)
+pic2 = Image.open('lena.png')
+
+weight, height = pic1.size
+
+for i in range(0, weight):
+  for j in range(0, height):
+    if pic2.getpixel((i,j)) == pic1.getpixel((i,j)):
+      pic1.putpixel((i,j),(255,255,255,0))
+
+pic1.save('pic_diff.png')
+
